82e14b6129d876934c4d4cdc7dc6a40f5d2c2029
Viktor <viktor@v.v>
2025-07-04 05:30:40 +0700

    up

--- frontend/src/services/apiService.tsx
+++ frontend/src/services/apiService.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 import { AuthManager } from '../utils/auth'
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL 
 
 // Интерфейс для AI метаданных
 interface AIMetadata {
